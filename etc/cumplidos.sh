ff47eb96a4b251c3ed262cc64115a11dfaef9600
SEAP UDEA <seapudea@gmail.com>
2015-12-18 14:01:05 -0500

    Commit

---
+++ etc/cumplidos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+lista=$(grep "tipocom" comisiones/*/*.txt | grep -v "noremunerada" | cut -f 1 -d ":")
+
+for comision in $lista
+do
+    comisionid=$(echo $comision | cut -f 2 -d '/')
+    if ! mysql -u root --password='pum' Comisiones -e "select comisionid,fechafin from Comisiones where comisionid='$comisionid' and fechafin>now()" | grep "-" &> /dev/null
+    then
+	echo "Copying empty file to $comisionid..."
+	cedula=$(grep "cedula =" comisiones/$comisionid/comision-$comisionid.txt | head -n 1 | awk -F" = " '{print $2}')
+	cp images/empty.pdf comisiones/$comisionid/Cumplido1_${cedula}_${comisionid}_empty.pdf
+    fi
+done
