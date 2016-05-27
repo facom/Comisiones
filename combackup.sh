9d42491c6b991a258c4ee1e6d70a3159f11c2218
Astronomia UdeA <astronomia.udea@gmail.com>
2016-05-26 19:47:57 -0500

    Commit

--- combackup.sh
+++ combackup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash                                                       
 echo "Dumping..."
-mysqldump -u root -p Comisiones > etc/data/comisiones.sql
-echo "Compressing..."
-tar cf etc/data/comisiones.tar etc/data/comisiones.sql comisiones
-p7zip etc/data/comisiones.tar
-# echo "Splitting..."
-# cd data/sql/dump
-# rm $filename.sql.7z-*
-# split -b 1024k ../$filename.sql.7z $filename.sql.7z-
-#cd - &> /dev/null
+filename="comisiones"
+#mysqldump -u root -p Comisiones > etc/data/comisiones.sql
+#echo "Compressing..."
+#tar cf etc/data/comisiones.tar etc/data/comisiones.sql comisiones
+#p7zip etc/data/comisiones.tar
+echo "Splitting..."
+cd etc/data/dump
+rm $filename.tar.7z-*
+split -b 1024k ../$filename.tar.7z $filename.tar.7z-
+cd - &> /dev/null
 echo "Git adding..."
-git add --all -f etc/data/*.tar.*
+git add --all -f etc/data/dump/*
 echo "Done."
